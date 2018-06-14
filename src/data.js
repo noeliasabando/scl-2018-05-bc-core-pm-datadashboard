cf1ee5d5c073d4463f35d1901fc687de4ab83310
Noelia Sabando <lolia_15@hotmail.com>
2018-06-14 01:47:22 -0400

    funciones de primer item

--- src/data.js
+++ src/data.js
@@ -1,17 +1,24 @@
+//let json= require('/data/cohorts/users.json');
 
 window.computeUsersStats = (users, progress, courses) => {
-  debugger
+  
   let lista = users.map(
     (user) => {
       user.stats = {
         percent : promedioCursos(progress[user.id], courses),
         exercises : {
           total: totalExcercises(progress[user.id], courses),
-          completed: true,
+          completed: usersExcercise(progress, courses),
           percent: true,
         },
-        reads: true,
-        quizzes: true,
+        reads: {
+          total: totalReads(progress, courses),
+          completed: completedReads(progress, courses),
+          percent: true,
+        },
+        quizzes: {
+          
+        }
       }
       return user
     }
@@ -26,19 +33,80 @@ function promedioCursos(progress, courses) {
   });
   return contador / courses.length;
 }
-
+//funciones ejercicios, total por curso, completados por alumna y FALTA porcentaje de completados por alumna
 function totalExcercises(progress, courses) {
   let total= 0; 
   courses.forEach(curso =>{
     Object.values(progress[curso].units).forEach(unit =>{
-      let partes = Object.values(unit.parts).filter(ejercicio => ejercicio.hasOwnProperty("exercises"));
-      partes.forEach((parte)=>{
+      let exercises = Object.values(unit.parts).filter(ejercicio => ejercicio.hasOwnProperty("exercises"));
+      exercises.forEach((parte)=>{
         total += Object.values(parte.exercises).length;
       })    
     })
   })
   return total;
 }
+
+function usersExcercise(progress, courses){
+  let total= 0;
+  courses.forEach(curso =>{
+    Object.values(progress[curso].units).forEach(unit =>{
+      let partes= Object.values(unit.parts).filter(ejercicio => ejercicio.hasOwnProperty("exercises"));
+      partes.forEach((parte)=>{
+        let completeExercices= Object.values(parte.exercises).filter(
+          (exercise)=>{
+            return exercise.completado === 1;
+          })
+        total += completeExercices.length;
+      })
+    })
+  })
+  return total;
+}
+
+function percentExcercise()
+
+function totalReads(progress, courses) {
+  let total=0;
+  courses.forEach(curso =>{
+    Object.values(progress[curso].units).forEach(unit =>{
+      let reads= Object.value(unit.parts).filter(lectura => lectura.hasOwnProperty("type")&& lectura.type==="read"); //al no poner llaves se retorna automaticamente la primera linea
+      total += reads.length
+    })
+  })
+  return total;
+}
+
+function completedReads(progress, courses) {
+  let total= 0;
+  courses.forEach(curso =>{
+    Object.values(progress[curso].units).forEach(unit =>{
+      let lecturas= Object.value(unit.parts).filter(lectura => lectura.type==="read");
+      let onlyReads= lecturas.filter((lectura)=> lectura.completed=== 1)
+      total+= onlyReads.length;
+    })
+  })
+  return total;
+}
+
+function percentExcercise(progress, courses) {
+  let total
+}
+
+function totalQuizzes(progress, courses){
+  let total= 0;
+  Object.entries(progress).forEach(([nombre, curso])=>{ 
+    if(courses.indexOf(nombre)>=0){
+      Object.values(curso.units).forEach((unit)=>{
+        let quiz=  Object.values(unit.parts).filter((part)=> part.type=== "quiz")   
+        total += quiz.length;   
+      })
+    }
+  })
+  return total;
+}
+    
+  
 
 
 var users = [
