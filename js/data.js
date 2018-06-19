54cf99757391ea87433da80b045b9a77e2cd0497
SorayaSM <soraya.sacaan@gmail.com>
2018-06-19 14:03:05 -0400

    actualizacion de los html y css

--- js/data.js
+++ js/data.js
@@ -1,76 +1,32 @@
-/*const users = require('users');
-const dataUsers = users.readFileSync("./data/cohorts/lim-2018-03-pre-core-pw/users.json", "utf8").toString();
-const progress = require('progress');
-const dataProgress = users.readFileSync("./data/cohorts/lim-2018-03-pre-core-pw/progress.json", "utf8").toString();
-*/
+users =
+    progress =
+    cohort =
 
-function getUsers() {
-    const users = new XMLHttpRequest();
-    users.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const dataUsers = JSON.parse(this.responseText);
-            for (i = 0; i < users.length[i]; i++) {
-
-            }
-        }
-    }
-};
-users.open("GET", "../data/cohorts/users.json", true);
-users.send();
-
-const progress = new XMLHttpRequest();
-users.open("GET", "../data/cohorts/progress.json", true);
-users.send();
-const courses = ["intro"];
-
-console.log(users);
-
-
-window.computeUsersStats = (users, progress, courses) => {
-    //debugger
-    let lista = users.map(
-        (user) => {
-            user.stats = {
-                percent: promedioCursos(progress[user.id], courses),
-                exercises: {
-                    total: totalExcercises(progress[user.id], courses),
-                    completed: true,
-                    percent: true,
-                },
-                reads: true,
-                quizzes: true,
-            }
-            return user;
-        }
-    )
-    return lista;
-}
-
-function promedioCursos(progress, courses) {
-    let contador = 0;
-    courses.forEach(curso => {
-        contador += progress[curso].percent;
-    });
-    return contador / courses.length;
-}
-
-function totalExcercises(progress, courses) {
-    let total = 0;
-    courses.forEach(curso => {
-        Object.values(progress[curso].units).forEach(unit => {
-            let partes = Object.values(unit.parts).filter(ejercicio => ejercicio.hasOwnProperty("exercises"));
-            partes.forEach((parte) => {
-                total += Object.values(parte.exercises).length;
-            })
-        })
+    fetch("../../data/cohorts/lim-2018-03-pre-core-pw/users.json")
+    .then(response => response.json())
+    .then(json => {
+        console.log(json); // => la data!!
     })
-    return total;
-}
+    .catch((err) => {
+        console.error(err);
+    });
 
-var richUsersData = window.computeUsersStats(users, progress, courses)
+
+fetch("../../data/cohorts/lim-2018-03-pre-core-pw/progress.json")
+    .then(response => response.json())
+    .then(json => {
+        console.log(json); // => la data!!
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 
-console.log(richUsersData)
-
-console.log(contents);
-Object.values(cohorts);
+fetch("../../data/cohorts/cohort.json")
+    .then(response => response.json())
+    .then(json => {
+        console.log(json); // => la data!!
+    })
+    .catch((err) => {
+        console.error(err);
+    });
