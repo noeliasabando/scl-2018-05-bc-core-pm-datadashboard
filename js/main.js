29c0e939e1042a04a63012c03ecd000bb02a4ee6
SorayaSM <soraya.sacaan@gmail.com>
2018-06-24 17:44:39 -0400

    otra vez  .

--- js/main.js
+++ js/main.js
@@ -32,27 +32,23 @@ function seleccion() {
     if (document.getElementById("nameord").selected == true) {
         let nombreOrdenado = window.sortUsers(userStats, "name", "ASC")
         imprimirLista(nombreOrdenado);
-        console.log("0");
     }
     if (document.getElementById("aveord").selected == true) {
         let avanceGral = window.sortUsers(userStats, "percent", "ASC")
         imprimirLista(avanceGral);
-        console.log("1");
     } else if (document.getElementById("btnejerCom").selected == true) {
         let ejerciciosOrdenados = window.sortUsers(userStats, "exercises percent", "ASC")
         imprimirLista(ejerciciosOrdenados);
-        console.log(" 2");
 
     } else if (document.getElementById("btnquizzes").selected == true) {
         let promPtosQuizzes = window.sortUsers(userStats, "quizzes scoreAvg", "ASC")
         imprimirLista(promPtosQuizzes);
-        console.log(" 3");
     }
 }
 
 let botonCohort = document.getElementById("btn")
 botonCohort.addEventListener("click", (event) => {
-    location = location;
+    window.location.reload(true);
 })
 
 function unhide() {
